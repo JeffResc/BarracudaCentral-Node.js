82655a5dc0bda99aeec9a751973463d33f7ee5e3
Feliks Toomsoo <feliks.toomsoo@gmail.com>
2019-10-09 00:13:48 +0300

    Remove request dependency

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-var request = require('request');
+var https = require('https');
 
 module.exports = {
-    lookup: function(ip, cb) {
-        var bytes = ip.split(".");
-        request('http://' + bytes[3] + '.' + bytes[2] + '.' + bytes[1] + '.' + bytes[0] + '.b.barracudacentral.org', function (error, response, body) {
+	lookup: function(ip, cb) {
+		var bytes = ip.split('.');
+		const request = https.get('https://' + bytes[3] + '.' + bytes[2] + '.' + bytes[1] + '.' + bytes[0] + '.b.barracudacentral.org');
+
+		request.on('error', (error) => {
 			if (error.syscall == 'getaddrinfo') {
-                cb('false');
-            } else if (error.syscall == 'connect') {
-                cb('true');
-            } else {
-                cb('error')
-            }
-        });
-    }
+				cb('false');
+			} else if (error.syscall == 'connect') {
+				cb('true');
+			} else {
+				cb('error');
+			}
+		});
+
+		request.on('end', () => cb('error'));
+	},
 };
