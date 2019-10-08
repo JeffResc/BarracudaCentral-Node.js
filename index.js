ad7018db693675e1d017fd4cdd044e172f0945b3
Feliks Toomsoo <feliks.toomsoo@gmail.com>
2019-10-09 00:16:49 +0300

    Convert tabs to spaces

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var https = require('https');
 
 module.exports = {
-	lookup: function(ip, cb) {
-		var bytes = ip.split('.');
-		const request = https.get('https://' + bytes[3] + '.' + bytes[2] + '.' + bytes[1] + '.' + bytes[0] + '.b.barracudacentral.org');
+    lookup: function(ip, cb) {
+        var bytes = ip.split('.');
+        const request = https.get('https://' + bytes[3] + '.' + bytes[2] + '.' + bytes[1] + '.' + bytes[0] + '.b.barracudacentral.org');
 
-		request.on('error', (error) => {
-			if (error.syscall == 'getaddrinfo') {
-				cb('false');
-			} else if (error.syscall == 'connect') {
-				cb('true');
-			} else {
-				cb('error');
-			}
-		});
+        request.on('error', (error) => {
+            if (error.syscall == 'getaddrinfo') {
+                cb('false');
+            } else if (error.syscall == 'connect') {
+                cb('true');
+            } else {
+                cb('error');
+            }
+        });
 
-		request.on('end', () => cb('error'));
-	},
+        request.on('end', () => cb('error'));
+    },
 };
